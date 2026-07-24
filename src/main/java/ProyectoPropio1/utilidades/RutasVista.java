e1547d0f52bef29c20ea1ddebc321ef75755236a
JoseD <josedanielportatil@gmail.com>
2026-07-23 19:26:13 -0500

    Separe los estilos css y los fxml en directorios especificos antes de pasar a trabajar en Punto de Venta

--- src/main/java/ProyectoPropio1/utilidades/RutasVista.java
+++ src/main/java/ProyectoPropio1/utilidades/RutasVista.java
@@ -2,38 +2,38 @@ package ProyectoPropio1.utilidades;
 
 public class RutasVista {
 
-    public static final String MENU_PRINCIPAL_VIEW = "/vista/MenuPrincipal.fxml";
-    public static final String ESTILOS_CSS_MENU_PRINCIPAL = "/css/estilosCssMenuPrincipal.css";
+    public static final String MENU_PRINCIPAL_VIEW = "/vista/gestionarTienda/MenuPrincipal.fxml";
+    public static final String ESTILOS_CSS_MENU_PRINCIPAL = "/css/gestionarTienda/estilosCssMenuPrincipal.css";
 
-    public static final String GESTIONAR_TIENDA_VIEW = "/vista/GestionarTienda.fxml";
-    public static final String ESTILOS_CSS_GESTIONAR_TIENDA = "/css/estilosCssGestionarTienda.css";
+    public static final String GESTIONAR_TIENDA_VIEW = "/vista/gestionarTienda/GestionarTienda.fxml";
+    public static final String ESTILOS_CSS_GESTIONAR_TIENDA = "/css/gestionarTienda/estilosCssGestionarTienda.css";
 
     public static final String GESTIONAR_CONFIGURACIONES_VIEW = "/vista/";
     public static final String ESTILOS_CSS_CONFIGURACIONES = "/css/";
 
-    public static final String GESTIONAR_DESCUENTOS_VIEW = "/vista/GestionDescuentos.fxml";
-    public static final String ESTILOS_CSS_DESCUENTOS = "/css/estilosCssDescuentos.css";
+    public static final String GESTIONAR_DESCUENTOS_VIEW = "/vista/gestionarTienda/GestionDescuentos.fxml";
+    public static final String ESTILOS_CSS_DESCUENTOS = "/css/gestionarTienda/estilosCssDescuentos.css";
 
-    public static final String GESTIONAR_IMPUESTOS_VIEW = "/vista/GestionImpuestos.fxml";
-    public static final String ESTILOS_CSS_IMPUESTOS = "/css/estilosCssImpuestos.css";
+    public static final String GESTIONAR_IMPUESTOS_VIEW = "/vista/gestionarTienda/GestionImpuestos.fxml";
+    public static final String ESTILOS_CSS_IMPUESTOS = "/css/gestionarTienda/estilosCssImpuestos.css";
 
-    public static final String GESTIONAR_SERVICIOS_VIEW = "/vista/GestionServicios.fxml";
-    public static final String ESTILOS_CSS_SERVICIOS = "/css/estilosCssServicios.css";
+    public static final String GESTIONAR_SERVICIOS_VIEW = "/vista/gestionarTienda/GestionServicios.fxml";
+    public static final String ESTILOS_CSS_SERVICIOS = "/css/gestionarTienda/estilosCssServicios.css";
 
-    public static final String GESTIONAR_INVENTARIOS_VIEW = "/vista/GestionInventarios.fxml";
-    public static final String ESTILOS_CSS_INVENTARIOS = "/css/estilosCssInventarios.css";
+    public static final String GESTIONAR_INVENTARIOS_VIEW = "/vista/gestionarTienda/GestionInventarios.fxml";
+    public static final String ESTILOS_CSS_INVENTARIOS = "/css/gestionarTienda/estilosCssInventarios.css";
 
-    public static final String GESTIONAR_PRODUCTOS_VIEW = "/vista/GestionProductos.fxml";
-    public static final String ESTILOS_CSS_PRODUCTOS = "/css/estilosCssProductos.css";
+    public static final String GESTIONAR_PRODUCTOS_VIEW = "/vista/gestionarTienda/GestionProductos.fxml";
+    public static final String ESTILOS_CSS_PRODUCTOS = "/css/gestionarTienda/estilosCssProductos.css";
 
-    public static final String CREAR_PRODUCTO_VIEW = "/vista/crearProducto.fxml";
-    public static final String ESTILOS_CSS_CREAR_PRODUCTOS = "/css/estilosCssCrearProductos.css";
+    public static final String CREAR_PRODUCTO_VIEW = "/vista/gestionarTienda/crearProducto.fxml";
+    public static final String ESTILOS_CSS_CREAR_PRODUCTOS = "/css/gestionarTienda/estilosCssCrearProductos.css";
 
-    public static final String EDITAR_ROPA_VIEW = "/vista/editarRopa.fxml";
-    public static final String ESTILOS_CSS_EDITAR_ROPA = "/css/estilosCssEditarRopaOPerecedero.css";
+    public static final String EDITAR_ROPA_VIEW = "/vista/gestionarTienda/editarRopa.fxml";
+    public static final String ESTILOS_CSS_EDITAR_ROPA = "/css/gestionarTienda/estilosCssEditarRopaOPerecedero.css";
 
-    public static final String EDITAR_PERECEDERO_VIEW = "/vista/editarPerecedero.fxml";
-    public static final String ESTILOS_CSS_EDITAR_PERECEDERO = "/css/estilosCssEditarRopaOPerecedero.css";
+    public static final String EDITAR_PERECEDERO_VIEW = "/vista/gestionarTienda/editarPerecedero.fxml";
+    public static final String ESTILOS_CSS_EDITAR_PERECEDERO = "/css/gestionarTienda/estilosCssEditarRopaOPerecedero.css";
 
 }
 
