706533e4d3c2f1d4575e5e394cab6e953883cc9f
JoseD <josedanielportatil@gmail.com>
2026-08-11 18:08:14 -0500

    Cree el fxml, el css y el controlador vacio para editar descuento

---
+++ src/main/java/RetailManagementSystem/vista/controladores/gestionarTienda/gestionarDescuentos/EditarDescuentoControlador.java
@@ -0,0 +1,13 @@
+package RetailManagementSystem.vista.controladores.gestionarTienda.gestionarDescuentos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class EditarDescuentoControlador {
+
+
+
+}
+
