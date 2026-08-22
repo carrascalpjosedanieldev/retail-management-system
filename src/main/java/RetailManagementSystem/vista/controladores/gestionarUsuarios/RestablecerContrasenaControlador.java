fab74cf272a6fd9a7baa3949cc251f42cc33a6d5
JoseD <josedanielportatil@gmail.com>
2026-08-21 20:24:34 -0500

    Cree la vista y el css de Restablecer Contrasena y su controlador vacio

---
+++ src/main/java/RetailManagementSystem/vista/controladores/gestionarUsuarios/RestablecerContrasenaControlador.java
@@ -0,0 +1,33 @@
+package RetailManagementSystem.vista.controladores.gestionarUsuarios;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class RestablecerContrasenaControlador {
+
+    @FXML
+    private Button btnCerrar;
+
+    @FXML
+    private Button btnCopiar;
+
+    @FXML
+    private Label lblContrasenaTemporal;
+
+    @FXML
+    private Label lblMensajeUsuario;
+
+    @FXML
+    void cerrarVentana(ActionEvent event) {
+
+    }
+
+    @FXML
+    void copiarContrasena(ActionEvent event) {
+
+    }
+
+}//===================================================================================================================//
+
